6381a5b5eb819376ff0b2775847841ee3266d545
a-regular-potato <ethanrn@outlook.com>
2022-05-05 23:04:15 +0100

    - Added Simple Controller  - Added Tiling to Water Shader  - Improved Ship (Destroyer) Model  - Added Propellers that animate

---
+++ Assets/DestinationSetter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DestinationSetter : MonoBehaviour
+{
+    public Camera PCam;
+
+    public bool LocationSetting;
+
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        PCam = GameObject.Find("Player").GetComponentInChildren<Camera>();
+    }
+    private void Update()
+    {
+        var ray = PCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (LocationSetting)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if(Physics.Raycast(ray, out hit))
+                {
+                    this.agent.SetDestination(new Vector3(hit.point.x, this.transform.position.y, hit.point.z));
+                    LocationSetting = false;
+                }
+            }
+        }
+    }
+}
